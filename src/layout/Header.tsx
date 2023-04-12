fdc72a7205312186ab28833bbf9130872732543f
tohoangkhoi <keepsmilingtg@gmail.com>
2023-04-12 17:56:03 +1000

    feat: create header

---
+++ src/layout/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import "./Header.css";
+export const Header = () => {
+  return (
+    <Grid className="avertro-header" container>
+      <img src="/Logo.png" />
+    </Grid>
+  );
+};
